901e7f1aa225d4a7917487579ee40dce15ef060b
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-09-26 21:24:16 -0600

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,67 +1,25 @@
-    public static void main(String[] args) {
-        }
-        for (int i = 0; i < 10; i++) {
-
-}
-            System.out.println("Random Number: " + item);
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
-        int[] data = new int[10];
-
-        }
-        for (int item : data) {
-public class RandomDataGenerator {            data[i] = (int) (Math.random() * 100) + 1;
-    }
-
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-}
-            System.out.println("Random Number: " + item);
-
-        }
-        }
-    public static void main(String[] args) {
-        for (int item : data) {
-        int[] data = new int[10];
-
 
     for item in data:
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-def main():
+
+import random
+def generate_random_data():
+
     data = generate_random_data()
+if __name__ == "__main__":
     main()
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-
-import random
-
-if __name__ == "__main__":
-def main():
-    return data
-    for item in data:
         print(f"Random Number: {item}")
-import random
-    data = generate_random_data()
-def generate_random_data():
+
     data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():        for (int i = 0; i < 10; i++) {
 
-
-    main()        data.push(Math.floor(Math.random() * 100) + 1);
-    data.forEach(item => {
-main();
-    for (let i = 0; i < 10; i++) {
-    const data = generateRandomData();
-function generateRandomData() {
-    });
-        console.log("Random Number: " + item);
-
+        int[] data = new int[10];
     }
 }
-
-    const data = [];
-
-function main() {
-    return data;
-}
+public class RandomDataGenerator {
+        }
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        }
