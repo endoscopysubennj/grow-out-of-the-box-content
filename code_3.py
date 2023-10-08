9d41da2bc1b6ff77fc451384962458791ba626be
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-08 10:40:53 -0600

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,37 +1,13 @@
-        for (int item : data) {
-}
-        }
-public class RandomDataGenerator {
-    }
-
-            System.out.println("Random Number: " + item);
-    public static void main(String[] args) {
-        int[] data = new int[10];
             data[i] = (int) (Math.random() * 100) + 1;
-        for (int i = 0; i < 10; i++) {
 
-        }            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+}
+    public static void main(String[] args) {
+    }
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
         for (int item : data) {
             System.out.println("Random Number: " + item);
-        }
-    }
-
-        for (int i = 0; i < 10; i++) {
-
-}
-public class RandomDataGenerator {
-        int[] data = new int[10];
-        }
-    public static void main(String[] args) {
-public class RandomDataGenerator {
-        }
-
-        }
-    }
-            System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
-        for (int item : data) {
-        int[] data = new int[10];
-}
-            data[i] = (int) (Math.random() * 100) + 1;
