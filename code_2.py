99a3c9c4855838a271351990be813549c2460d65
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-09 23:40:38 -0600

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,40 +1,39 @@
-    return data
-def main():
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        int[] data = new int[10];
+}
+        }
+            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+    }
+        }
 
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
+        for (int item : data) {
 import random
 
-        print(f"Random Number: {item}")
+    main()
+
     for item in data:
 def generate_random_data():
     data = generate_random_data()
-    main()    main()
-    data = generate_random_data()
-if __name__ == "__main__":
+
     return data
+        print(f"Random Number: {item}")
+
+def main():
     data = [random.randint(1, 100) for _ in range(10)]
-import random
-
-        print(f"Random Number: {item}")
-    for item in data:
-def main():
-
-
-
-def generate_random_data():
-def main():
-if __name__ == "__main__":
-
-        print(f"Random Number: {item}")
-
-import random
-
-    data = generate_random_data()
-    return data
-def generate_random_data():
+if __name__ == "__main__":        print(f"Random Number: {item}")
     main()
     for item in data:
+
+    data = generate_random_data()
+
+if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+def main():
+
+def generate_random_data():
+    return data
