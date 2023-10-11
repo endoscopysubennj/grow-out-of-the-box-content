eb4668b04b19ecbcbbe3acc8bf22a814def9f4aa
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-10 18:08:31 -0600

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,16 +1,13 @@
-    }
+    main()
+    for item in data:
 
-    const data = generateRandomData();
-    data.forEach(item => {
-}
-}
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+def generate_random_data():
+    return data
+    data = generate_random_data()
 
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-main();
-function generateRandomData() {
-    const data = [];
-    });
-    for (let i = 0; i < 10; i++) {
-    return data;
+import random
+
+def main():
