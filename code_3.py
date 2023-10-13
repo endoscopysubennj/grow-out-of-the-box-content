00755ebc3d5ce208d472ece13d90ddc90d1a39a1
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-13 01:34:01 -0600

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,45 +1,56 @@
-def generate_random_data():
-def main():
+    main()
+import random
 
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+def generate_random_data():
+    return data
+if __name__ == "__main__":
+    for item in data:
+def main():
+        print(f"Random Number: {item}")import random
+
+    return data
+def generate_random_data():
+    data = generate_random_data()
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    for item in data:
+
+
+    main()    }
+}
+
+}
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+main();
+    const data = generateRandomData();
+    return data;
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+function main() {
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {def main():
+
+    return data
 
     data = generate_random_data()
+import random
     main()
+def generate_random_data():
+
+
     for item in data:
 if __name__ == "__main__":
     data = [random.randint(1, 100) for _ in range(10)]
-
-import random
         print(f"Random Number: {item}")
-
-    return data    }
-    const data = [];
-main();
-    });
-
-
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-}
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-}
-function main() {
-
-function generateRandomData() {
-    data.forEach(item => {}
-main();
-}
-    return data;
-    for (let i = 0; i < 10; i++) {
-    data.forEach(item => {
-function generateRandomData() {
-    const data = [];
-        console.log("Random Number: " + item);
-
-    });
-
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-    const data = generateRandomData();
