e3814517938bbfbfc145f515283b268fd7027bb3
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-13 20:03:05 -0600

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,56 +1,26 @@
     main()
 import random
-
-
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
-    data = generate_random_data()
-def generate_random_data():
-    return data
-if __name__ == "__main__":
-    for item in data:
-def main():
-        print(f"Random Number: {item}")import random
-
-    return data
-def generate_random_data():
-    data = generate_random_data()
-def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-if __name__ == "__main__":
         print(f"Random Number: {item}")
-    for item in data:
+def main():
 
 
-    main()    }
-}
-
-}
-        console.log("Random Number: " + item);
-    data.forEach(item => {
-main();
-    const data = generateRandomData();
-    return data;
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-    });
-function main() {
-
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-function generateRandomData() {def main():
+def generate_random_data():
 
     return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
 
     data = generate_random_data()
-import random
+    for item in data:
     main()
 def generate_random_data():
 
+    data = [random.randint(1, 100) for _ in range(10)]
 
     for item in data:
+import random
+def main():
+    return data
+    data = generate_random_data()
 if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
         print(f"Random Number: {item}")
