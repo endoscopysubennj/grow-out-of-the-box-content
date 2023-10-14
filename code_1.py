0f3c2a0a32580206c32f34727ee945d8efbdd86c
endoscopysubennj <139906984+endoscopysubennj@users.noreply.github.com>
2023-10-14 14:32:05 -0600

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,28 @@
-    main()
-    for item in data:
+        int[] data = new int[10];
 
-    data = [random.randint(1, 100) for _ in range(10)]
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-def generate_random_data():
-    return data
-    data = generate_random_data()
+}
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int item : data) {
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
 
-import random
+    }    const data = [];
 
-def main():
+function main() {
+    });
+}
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+
+}
+    }
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+function generateRandomData() {
